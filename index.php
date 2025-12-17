5c580b63a529ba2e8e6667b5c3ef24c72bafac0e
Léo Lesimple <leolesimple@icloud.com>
2025-12-17 11:45:50 +0100

    Ajout du routage des pages.

--- index.php
+++ index.php
@@ -1,13 +1,33 @@
-<!doctype html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+<?php
+require __DIR__ . '/includes/config.php';
 
-</body>
-</html>
+include __DIR__ . '/includes/nav.php';
+$basePath = '/loty';
+
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+if (str_starts_with($uri, $basePath)) {
+    $uri = substr($uri, strlen($basePath));
+}
+
+$uri = trim($uri, '/');
+
+if ($uri === '') {
+    $page = 'home';
+} else {
+    $page = $uri;
+}
+
+$page = str_replace('..', '', $page);
+
+$file = __DIR__ . '/pages/' . $page . '.php';
+
+if (file_exists($file)) {
+    require $file;
+    exit;
+}
+
+http_response_code(404);
+require __DIR__ . '/pages/404.php';
+
+include __DIR__ . '/includes/footer.php';
---
+++ pages/404.php
@@ -0,0 +1,9 @@
+<?php
+http_response_code(404);
+?>
+<section class="container" style="padding: 4rem 1rem; text-align:center;">
+    <h1>404 - Page non trouvée</h1>
+    <p>La page que vous recherchez n'existe pas.</p>
+    <p><a href="/">Retour à l'accueil</a></p>
+</section>
+
