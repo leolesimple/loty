71cdc5e4f7e8399adcc704121bf6f31e7580c357
Léo Lesimple <leolesimple@icloud.com>
2025-12-27 18:19:08 +0100

    feat: (podium) - Ajout de la page du podium de la semaine. (à venir)
    Signed-off-by: Léo Lesimple <leolesimple@icloud.com>

---
+++ pages/podium.php
@@ -0,0 +1,9 @@
+<h1>
+    La sélection de la semaine
+</h1>
+<p>
+    <strong>
+        Bientôt disponible !
+    </strong>
+    Retrouvez chaque semaine le résultat du vote de la communauté pour élire le Bento et la Recette de la semaine.
+</p>
